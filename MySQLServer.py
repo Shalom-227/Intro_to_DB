756796b46a93b11d23c16a98595ce61796729e33
Shalom <alaladeshalom02@gmail.com>
2025-02-02 14:07:47 +0100

    simple python script that creates the database alx_book_store in MySQL server.

---
+++ MySQLServer.py
@@ -0,0 +1,27 @@
+#!/bin/Python3
+
+import mysql.connector
+
+from mysql.connector import Error
+
+try:
+	db_connection = mysql.connector.connect(
+	host ="localhost",
+	user ="root",
+	password ="Comforter"
+	)
+
+	if db_connection.is_connected():
+		cursor_obj = db_connection.cursor()
+
+	cursor_obj.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+	print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+	print(f"Error, failing to connect to the MySQL Server: {e}")
+
+finally:
+	if "cursor_obj" in locals():
+		cursor_obj.close()
+	if "db_connection" in locals() and db_connection.is_connected():
+		db_connection.close()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 0. alx_book_store.sql simulates a bookstore database that stores information about books, authors, customers, orders, and order details.
+1. MySQLServer.py a simple python script that creates the database alx_book_store in the MySQL server, incoporating error handling techniques and closing of database.
