30c5e167c727a73c051afdf830888e25c52346b2
Shalom <alaladeshalom02@gmail.com>
2025-02-02 15:49:28 +0100

    script prints all information about a table

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,7 @@
 --script that prints the full description of the table books from the database alx_book_store in your MySQL server.
 
 USE alx_book_store;
-SELECT * FROM books;
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+
